bb6c8d0694af7d402480378a16e1036765182763
shmiitg <shubhamank002@gmail.com>
2022-03-10 13:11:10 +0530

    404 page updated

--- pages/404.js
+++ pages/404.js
@@ -7,20 +7,22 @@ const NotFound = () => {
 
     useEffect(() => {
         setTimeout(() => {
-            // router.go(-1);
+            // router.go(-1)
+            // router.go(1)
             router.push("/");
         }, 3000);
     }, []);
 
     return (
         <div className="not-found">
-            <h1>Oooops...</h1>
-            <h2>That page cannot be found</h2>
+            <h1>Ooops...</h1>
+            <h2>That page cannot be found :(</h2>
             <p>
-                Go back to the{" "}
+                Going back to the{" "}
                 <Link href="/">
                     <a>Homepage</a>
                 </Link>{" "}
+                in 3 seconds...
             </p>
         </div>
     );
